11fccbc10292d25b9df15c9d78933cf3862e7cf9
Sam Deane <sam@elegantchaos.com>
2020-05-20 17:13:22 +0100

    Use latest logger.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Bundles.git", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/DictionaryCoding.git", from: "1.0.9"),
-        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.5.3"),
+        .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.5.5"),
         .package(url: "https://github.com/elegantchaos/Hardware.git", from: "1.0.0")
     ],
     targets: [
---
+++ Sources/ActionStatusCore/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sam Deane on 20/05/2020.
+//
+
+import Foundation
