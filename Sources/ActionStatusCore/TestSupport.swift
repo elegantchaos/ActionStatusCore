ce060af8c031270764936372eefe795a20f901e4
Sam Deane <sam@elegantchaos.com>
2020-05-20 19:33:22 +0100

    Don't require UIKit.

--- Sources/ActionStatusCore/TestSupport.swift
+++ Sources/ActionStatusCore/TestSupport.swift
@@ -3,15 +3,6 @@
 //  All code (c) 2020 - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-import UIKit
-
-public extension UIApplication {
-    var isTestingUI : Bool {
-        guard let testingFlag = ProcessInfo.processInfo.environment["UITesting"], testingFlag == "YES" else { return false }
-        return true
-    }
-}
-
 public extension Dictionary where Key == String, Value == String {
     var isTestingUI: Bool {
         get { self["UITesting"] == "YES" }
