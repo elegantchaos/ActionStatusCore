0e1886f7c07eff9b1893a7d1e380d22513647175
Sam Deane <sam@elegantchaos.com>
2020-05-21 16:11:07 +0100

    Expanded the test model a bit.

--- Sources/ActionStatusCore/TestModel.swift
+++ Sources/ActionStatusCore/TestModel.swift
@@ -7,10 +7,19 @@ public class TestModel: Model {
     public let repos: [Repo]
     
     public init() {
+        var datastoreRepo = Repo("Datastore", owner: "elegantchaos", workflow: "Swift", state: .passing)
+        datastoreRepo.settings = WorkflowSettings(options: ["macOS", "iOS", "linux", "swift-52", "swift-53", "release", "build", "test", "header"])
+        
         repos = [
+            Repo("Actions", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            Repo("ActionsKit", owner: "elegantchaos", workflow: "Tests", state: .passing),
             Repo("ApplicationExtensions", owner: "elegantchaos", workflow: "Tests", state: .failing),
-            Repo("Datastore", owner: "elegantchaos", workflow: "Swift", state: .passing),
-            Repo("DatastoreViewer", owner: "elegantchaos", workflow: "Build", state: .failing),
+            Repo("BindingsExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            Repo("Builder", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            Repo("CollectionExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            Repo("CommandShell", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            datastoreRepo,
+            Repo("Hardware", owner: "elegantchaos", workflow: "Build", state: .failing),
             Repo("Logger", owner: "elegantchaos", workflow: "tests", state: .unknown),
             Repo("ViewExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing),
         ]
